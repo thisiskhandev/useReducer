820186a4f5ba563e8fca9f99e06640f858ce22a3
Hassan Khan <hassankhan427726@gmail.com>
2022-10-12 23:34:10 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Button } from "react-bootstrap";
+export default function App() {
+  return (
+    <React.Fragment>
+      <Button variant="dark">Dark</Button>
+    </React.Fragment>
+  );
+}
